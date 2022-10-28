81d032c675c0a04a8df6b5069c6f4e443a3601c8
Krymchuc <khrystyna.krymchuk@oa.edu.ua>
2022-10-28 23:59:44 +0300

    Game demo

---
+++ Animation_Demo_3D_Lab5/Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+   public Animator animator;
+    public MovePerson movePerson;
+    public CharacterStatus characterStatus;
+    
+    public void UpdateAnimation()
+    {
+        animator.SetBool("sprint", characterStatus.isSprint);
+        animator.SetBool("aiming", characterStatus.isAiming);
+
+        if(!characterStatus.isAiming)
+        {
+            AnimationNormal();
+        }
+        else
+        {
+            AnimationAiming();
+        }
+    }
+
+    void AnimationNormal()
+    {
+        if (movePerson.moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+        {
+            Walk();
+        }
+        if (movePerson.moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+        {   
+            Run();            
+        }
+        else if (movePerson.moveDirection == Vector3.zero)
+        {
+            Idle();
+        }
+    }
+
+    void AnimationAiming()
+    {
+        float ver = movePerson.vertical;
+        float hor = movePerson.horizontal;
+
+        animator.SetFloat("vertical", ver, 0.15f, Time.deltaTime);
+        animator.SetFloat("horizontal", hor, 0.15f, Time.deltaTime);
+    }
+
+    private void Idle()
+    {
+        animator.SetFloat("vertical", 0f, 0.1f, Time.deltaTime);
+    }
+    private void Walk()
+    {
+        movePerson.moveAmount = 5;
+        animator.SetFloat("vertical", 0.5f, 0.1f, Time.deltaTime);
+    }
+    private void Run()
+    {
+        movePerson.moveAmount = 10;
+        animator.SetFloat("vertical", 1, 0.1f, Time.deltaTime);
+    }
+
+}
